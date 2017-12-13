8c4831320c1bf398d88a38fcdfbc0246217900dc
SergioPow <sergiopgomes92@gmail.com>
2017-12-13 02:01:39 -0200

    Creating the applicaion

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# vuejs-starwars-quotes
-vuejs starwars quotes
+# VueJS Star Wars Quotes
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <app-header v-bind:maxQuotes="maxQuotes" v-bind:quoteCount="quotes.length"></app-header>
+        <app-new-quote v-on:quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid v-bind:quotes="quotes" v-on:quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on a Quote to delete it!
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                    'May the force be with you',
+                    'I have the high ground',
+                    'I am your father',
+                    'Every thing is going as planned'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >=this.maxQuotes) {
+                    return alert('Please delete some Quote first!');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        }
+    }
+</script>
+
+<style>
+</style>
